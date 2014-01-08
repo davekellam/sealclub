0188692b860a5d3ff9761fdd2033977fb4a02b02
Dave Kellam <dave@eightface.com>
2014-01-08 03:49:26 -0500

    Removing page-linking and password/comment crap from content module

--- content.php
+++ content.php
@@ -22,12 +22,6 @@
 	<?php else : ?>
 	<div class="entry-content">
 		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'sealclub' ) ); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'sealclub' ),
-				'after'  => '</div>',
-			) );
-		?>
 	</div><!-- .entry-content -->
 	<?php endif; ?>
 
@@ -53,11 +47,5 @@
 			</span>
 			<?php endif; // End if $tags_list ?>
 		<?php endif; // End if 'post' == get_post_type() ?>
-
-		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'sealclub' ), __( '1 Comment', 'sealclub' ), __( '% Comments', 'sealclub' ) ); ?></span>
-		<?php endif; ?>
-
-		<?php edit_post_link( __( 'Edit', 'sealclub' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-meta -->
 </article><!-- #post-## -->
