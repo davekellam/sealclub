2706fb6c733a14874446916cf4f11ba6f14e728a
Dave Kellam <dave@eightface.com>
2014-01-08 02:22:57 -0500

    Remove sidebar widget init

--- functions.php
+++ functions.php
@@ -58,21 +58,6 @@ endif; // sealclub_setup
 add_action( 'after_setup_theme', 'sealclub_setup' );
 
 /**
- * Register widgetized area and update sidebar with default widgets.
- */
-function sealclub_widgets_init() {
-	register_sidebar( array(
-		'name'          => __( 'Sidebar', 'sealclub' ),
-		'id'            => 'sidebar-1',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-	) );
-}
-add_action( 'widgets_init', 'sealclub_widgets_init' );
-
-/**
  * Enqueue scripts and styles.
  */
 function sealclub_scripts() {
