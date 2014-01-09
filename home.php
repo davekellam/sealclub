a23796ae85938e5a0ec49d31c95f683881f4de0f
Dave Kellam <dave@eightface.com>
2014-01-09 02:09:06 -0500

    Adding home page template

---
+++ home.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The homepage template
+ *
+ * @package Seal Club
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php the_title(); ?>
+
+			<?php endwhile; ?>
+
+			<?php sealclub_paging_nav(); ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'content', 'none' ); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
